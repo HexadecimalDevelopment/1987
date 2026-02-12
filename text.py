62707ddde4bf7727c747b618e2ebafd3c93d5523
HexadecimalDevelopment <huntjburke@gmail.com>
2026-02-12 15:00:12 -0500

    Updated text.py

--- text.py
+++ text.py
@@ -2,17 +2,17 @@ import cbc, sys
 
 try:
 
-    cbc.out('You have been hacked...', terminator='')
+    cbc.out('You could have been hacked...', terminator='')
     print(' Press enter to continue:', end='')
     input()
-    cbc.out('You got lucky... This time.....', terminator='')
+    cbc.out('This was just a harmless script designed as a prank, but next time might not be...', terminator='')
     input()
-    cbc.out('This was just a harmless script, but next time might not be...', terminator='')
+    cbc.out('Online security is IMPORTANT.', terminator='')
     input()
-    cbc.out("So secure your system... Before it's too late.....", terminator='')
+    cbc.out('In 2023, there were over 6 billion malware attacks globally.', terminator='')
     input()
-    cbc.out('Goodbye... For now.....')
-    input()
+    cbc.out('https://www.cisa.gov/secure-our-world')
+    input('Press enter to exit.')
     sys.exit()
 
 except KeyboardInterrupt: sys.exit()
